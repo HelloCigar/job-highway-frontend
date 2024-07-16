fde9da87e93cbce33d7f729ecd23a5fccf414e2b
HelloCigar? <jckarim@up.edu.ph>
2024-07-16 21:28:38 +0800

    fixed custom menu to be mobile friendly

--- components/Custom/Menu.vue
+++ components/Custom/Menu.vue
@@ -3,7 +3,7 @@ const userStore = useUserStore()
 </script>
 
 <template>
-    <div class="container mx-auto px-24">
+    <div class="container mx-auto">
         <nav
             class="block w-full max-w-screen-xl px-4 py-2 mx-auto text-white bg-white border shadow-md rounded-xl border-white/80 bg-opacity-80 backdrop-blur-2xl backdrop-saturate-200 lg:px-8 lg:py-4">
             <div class="container flex items-center justify-between mx-auto text-blue-gray-900">
@@ -20,14 +20,14 @@ const userStore = useUserStore()
                 <div class="flex items-center gap-x-3">
                     <NuxtLink to="/">
                         <button
-                            class="hidden px-4 py-2 font-sans text-xs font-bold text-center text-gray-900 uppercase align-middle transition-all rounded-lg select-none hover:bg-gray-900/10 active:bg-gray-900/20 disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none lg:inline-block"
+                            class="inline-block px-4 py-2 font-sans text-xs font-bold text-center text-gray-900 uppercase align-middle transition-all rounded-lg select-none hover:bg-gray-900/10 active:bg-gray-900/20 disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none lg:inline-block"
                             type="button">
                             <span>Home</span>
                         </button>
                     </NuxtLink>
                     <NuxtLink to="/browse">
                         <button
-                            class="hidden px-4 py-2 font-sans text-xs font-bold text-center text-gray-900 uppercase align-middle transition-all rounded-lg select-none hover:bg-gray-900/10 active:bg-gray-900/20 disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none lg:inline-block"
+                            class="inline-block px-4 py-2 font-sans text-xs font-bold text-center text-gray-900 uppercase align-middle transition-all rounded-lg select-none hover:bg-gray-900/10 active:bg-gray-900/20 disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none lg:inline-block"
                             type="button">
                             <span>Browse</span>
                         </button>
