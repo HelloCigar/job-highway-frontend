2f94ef240a534dca634d96e30df2e58bff1061a5
HelloCigar? <jckarim@up.edu.ph>
2024-07-18 09:46:16 +0800

    allow updating of category of jobs

--- pages/editjob/[id].vue
+++ pages/editjob/[id].vue
@@ -14,7 +14,9 @@ onMounted(() => {
 })
 
 const { data: job, status } = await useFetch(`${apiUrl}/api/v1/jobs/${id}/`)
+const { data: jobCategories } = await useFetch(`${apiUrl}/api/v1/jobs/categories/`)
 
+let category = ref(job?.value.category)
 let title = ref(job?.value.title)
 let description = ref(job?.value.description)
 let position_salary = ref(job?.value.position_salary)
@@ -59,7 +61,7 @@ async function submitForm() {
     await $fetch(`${apiUrl}/api/v1/jobs/${id}/edit/`, {
         method: 'PUT',
         body: {
-            category: job?.value.category,
+            category: category.value,
             title: title.value,
             description: description.value,
             position_salary: position_salary.value,
@@ -97,15 +99,34 @@ useSeoMeta({
 
 
 <template>
-    <div class="flex justify-center py-3">
-        <div class="relative flex flex-col text-gray-700 bg-transparent shadow-none rounded-xl bg-clip-border">
+    <div class="flex justify-center py-6">
+        <div
+            class="flex flex-col px-6 md:px-24 text-gray-700 bg-transparent shadow-none rounded-xl bg-clip-border w-full">
             <h4
                 class="block font-sans text-2xl antialiased font-semibold leading-snug tracking-normal text-blue-gray-900">
                 Edit Job
             </h4>
-            <form @submit.prevent="submitForm" class="max-w-screen-lg mt-3 mb-2 w-full">
-                <div class="flex justify-center gap-3 mb-1">
-                    <div class=" flex flex-col gap-3 mb-1">
+            <form @submit.prevent="submitForm" class="mt-3 mb-2 w-full">
+                <div class="flex flex-col md:flex-row justify-center gap-3 mb-1">
+                    <div class=" flex flex-col gap-3 mb-1 w-full">
+                        <h6
+                            class="block -mb-3 font-sans text-base antialiased font-semibold leading-relaxed tracking-normal text-blue-gray-900">
+                            Category
+                        </h6>
+                        <div class="relative h-11 w-full min-w-[200px]">
+                            <select v-model="category" class="peer h-full w-full rounded-md border border-blue-gray-200 border-t-transparent
+                        !border-t-blue-gray-200 bg-transparent px-3 py-3 font-sans text-sm font-normal
+                        text-blue-gray-700 outline outline-0 transition-all placeholder-shown:border
+                        placeholder-shown:border-blue-gray-200 placeholder-shown:border-t-blue-gray-200 focus:border-2
+                        focus:border-gray-900 focus:border-t-transparent focus:!border-t-gray-900 focus:outline-0
+                        disabled:border-0 disabled:bg-blue-gray-50">
+
+                                <option value="">Select category</option>
+                                <option v-for="(category, index) in jobCategories" :key="index" :value="category?.id">{{
+                                    category?.title }}
+                                </option>
+                            </select>
+                        </div>
                         <h6
                             class="block -mb-3 font-sans text-base antialiased font-semibold leading-relaxed tracking-normal text-blue-gray-900">
                             Title
@@ -134,13 +155,13 @@ useSeoMeta({
                             Salary
                         </h6>
                         <div class="relative h-11 w-full min-w-[200px]">
-                            <input v-model="position_salary"
+                            <input v-model="position_salary" placeholder="e.g 20K-30K"
                                 class="peer h-full w-full rounded-md border border-blue-gray-200 border-t-transparent !border-t-blue-gray-200 bg-transparent px-3 py-3 font-sans text-sm font-normal text-blue-gray-700 outline outline-0 transition-all placeholder-shown:border placeholder-shown:border-blue-gray-200 placeholder-shown:border-t-blue-gray-200 focus:border-2 focus:border-gray-900 focus:border-t-transparent focus:!border-t-gray-900 focus:outline-0 disabled:border-0 disabled:bg-blue-gray-50" />
                             <label
                                 class="before:content[' '] after:content[' '] pointer-events-none absolute left-0 -top-1.5 flex h-full w-full select-none !overflow-visible truncate text-[11px] font-normal leading-tight text-gray-500 transition-all before:pointer-events-none before:mt-[6.5px] before:mr-1 before:box-border before:block before:h-1.5 before:w-2.5 before:rounded-tl-md before:border-t before:border-l before:border-blue-gray-200 before:transition-all before:content-none after:pointer-events-none after:mt-[6.5px] after:ml-1 after:box-border after:block after:h-1.5 after:w-2.5 after:flex-grow after:rounded-tr-md after:border-t after:border-r after:border-blue-gray-200 after:transition-all after:content-none peer-placeholder-shown:text-sm peer-placeholder-shown:leading-[4.1] peer-placeholder-shown:text-blue-gray-500 peer-placeholder-shown:before:border-transparent peer-placeholder-shown:after:border-transparent peer-focus:text-[11px] peer-focus:leading-tight peer-focus:text-gray-900 peer-focus:before:border-t-2 peer-focus:before:border-l-2 peer-focus:before:!border-gray-900 peer-focus:after:border-t-2 peer-focus:after:border-r-2 peer-focus:after:!border-gray-900 peer-disabled:text-transparent peer-disabled:before:border-transparent peer-disabled:after:border-transparent peer-disabled:peer-placeholder-shown:text-blue-gray-500"></label>
                         </div>
                     </div>
-                    <div class="flex flex-col gap-3 mb-1">
+                    <div class="flex flex-col gap-3 mb-1 w-full">
                         <h6
                             class="block -mb-3 font-sans text-base antialiased font-semibold leading-relaxed tracking-normal text-blue-gray-900">
                             Location
